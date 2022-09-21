3b3765575c6da0fa56c9d3e3ea67f0c05b5f87a2
dbres <3077302585@qq.com>
2022-09-21 11:11:23 +0800

    10

---
+++ assets/AboutView.6f65c4d0.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index.825f0e03.js";const _={},c={class:"about"},n=o("h1",null,"This is an about sd",-1),a=[n];function i(r,d){return t(),s("div",c,a)}const l=e(_,[["render",i]]);export{l as default};
