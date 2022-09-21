a8a341f5c85d8fc892b97c91c84f59b46977b1ce
dbres <3077302585@qq.com>
2022-09-21 17:57:26 +0800

    ss

--- README.md
+++ README.md
@@ -26,4 +26,6 @@ npm run dev
 
 ```sh
 npm run build
+git add dist
+git subtree push --prefix dist origin gh-pages
 ```
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,8 +11,8 @@
 <script>
   import {Viewer} from 'photo-sphere-viewer'
   import 'photo-sphere-viewer/dist/photo-sphere-viewer.css'
-  import sphere from '@/assets/image/sphere.jpg'
-  import s300 from '@/assets/image/300.jpg'
+  // import sphere from '@/assets/image/sphere.jpg'
+  // import s300 from '@/assets/image/300.jpg'
   export default {
     data () {
       return {
@@ -23,11 +23,11 @@
         let that = this
         this.view = new Viewer({
             container:document.querySelector('#viewer'),
-            panorama:sphere,
-            // panorama:'https://photo-sphere-viewer-data.netlify.app/assets/sphere.jpg',
+            // panorama:sphere,
+            panorama:'https://photo-sphere-viewer-data.netlify.app/assets/sphere.jpg',
             size:{
                 width: '100%',
-                height: '100vh',
+                height: '100vh', 
             },
             autorotateDelay: 0,
             autorotateIdle: true,
