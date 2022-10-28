c6b614b530592a5b9e81ea005001c9899fd6bab2
dbres <3077302585@qq.com>
2022-10-28 17:51:11 +0800

    no message

---
+++ src/components/nav.vue
@@ -0,0 +1,16 @@
+<style lang="scss">
+    #navBox{
+        width: 80%;
+        background-color: white;
+    }
+</style>
+
+<template>
+    <div id="navBox" class="pd-10">
+        
+    </div>
+</template>
+
+<script>
+
+</script>
