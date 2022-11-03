753f4c80a49867f15a172ab1197763bd8b4433ed
dbres <3077302585@qq.com>
2022-11-03 17:58:58 +0800

    something

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,8 +8,13 @@
     position: relative;
     width: 100%;
     // height: 100%;
-    height: calc(100vh - 60px);;
+    height: calc(100vh - 60px);
     z-index: 2;
+  }
+  .frame{
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 }
 </style>
@@ -17,10 +22,10 @@
   <div id="homeBox">
     <photoSphere :background="true"></photoSphere>
     <navT></navT>
-    <div class="content mg-t-10">
-      <contentList class="mg-r-10 mg-l-10" style="flex-grow: 6;" v-if="type == 'xcx' || type == 'game'"></contentList>
-      <div class="mg-r-10 mg-l-10" style="flex-grow: 6;" v-else>
-        <iframe src="https://db-res.github.io/move/" class="show" frameborder="0"></iframe>
+    <div class="content mg-t-10" style="flex-grow: 6;">
+      <contentList class="mg-r-10 mg-l-10 W100" v-show="type == 'xcx' || type == 'game'"></contentList>
+      <div class="mg-r-10 mg-l-10 W100" v-show="type == 'three'">
+        <iframe style="background-color: #eeeeee;" src="https://db-res.github.io/move/" loading class="show frame" frameborder="0"></iframe>
       </div>
       <taskList class="mg-r-10" style="flex-grow: 1;"></taskList>
     </div>
